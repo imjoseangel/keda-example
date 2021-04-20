d3f9b2b159093b4295f7f28bede4faaa7e97cac5
imjoseangel <josea.munoz@gmail.com>
2021-04-20 08:44:53 +0200

    add redis helper

---
+++ helper/go.mod
@@ -0,0 +1,8 @@
+module helper
+
+go 1.16
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/urfave/cli v1.22.5 // indirect
+)
---
+++ helper/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "app",
+		Usage: "Simple app for a keda related talk",
+		Commands: []cli.Command{
+			{
+				Name:  "redis",
+				Usage: "redis methods",
+				Subcommands: []cli.Command{
+					{
+						Name:  "publish",
+						Usage: "Publishes messages to Redis list",
+						Action: func(c *cli.Context) error {
+							result, err := Publish()
+							if err != nil {
+								fmt.Println("Failed to publish messages")
+								log.Fatal(err)
+							} else {
+								fmt.Printf("Published messages, actual list len: %d\n", result)
+							}
+							return nil
+						},
+					},
+					{
+						Name:  "drain",
+						Usage: "Drains the Redis list",
+						Action: func(c *cli.Context) error {
+							_, err := Drain()
+							if err != nil {
+								fmt.Println("Failed to drain Redis list")
+								log.Fatal(err)
+							} else {
+								fmt.Println("Queue drained.")
+							}
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
